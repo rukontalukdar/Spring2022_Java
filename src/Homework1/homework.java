795c4c161f10a08f7f0a567b8896348d7aa1d190
Prio T <prio@Prios-MacBook-Air.local>
2022-03-05 04:26:19 -0500

    My homework

---
+++ src/Homework1/homework.java
@@ -0,0 +1,59 @@
+package Homework1;
+
+public class homework {
+    public static void main(String[] args) {
+
+
+        /**
+         *
+         * Numbers of hours in a day
+         * max numbers of days in a year
+         * total numbers of employees in an organization
+         * population in a country
+         * mortgage rate
+         * balance in a bank account
+         * Does the sunrise from the west?
+         * Initial of your name (Use char)
+         * Store your full name ( Use string)
+         *
+         */
+
+//Question 1: Numbers of hours in a day
+
+        byte numbersOfHoursInADay = 24;
+        System.out.println("There is " + (numbersOfHoursInADay) + " hours in a day");
+
+//Question 2: max numbers of days in a year
+        int maxNumbersOfDaysInYear = 365;
+        System.out.println("There is max of " + (maxNumbersOfDaysInYear) + " days in a year");
+
+        //question 3 : total numbers of employees in an organization
+        int numbersOfEmployeeInTesla = 99290;
+        System.out.println("There are currently " + (numbersOfEmployeeInTesla) + " employee in tesla");
+
+        //question 4 :population in a country
+        long populationInUSA = 331_449_281L;
+        System.out.println("There are " + (populationInUSA) + " people in USA");
+
+        //Question 5: mortgage rate
+        double myMortgageRate = 3.9;
+        System.out.println("My mortgage rate is " + (myMortgageRate));
+
+        //Question 6: balance in a bank account
+        long balanceInMyBank = 100_000_000L;
+        System.out.println("I have " + (balanceInMyBank) + " in my account");
+
+        //Question 7: Does the sunrise from the west?
+        boolean sunRiseFromWest = false;
+        System.out.println("Does the sun rise from the west? " + (sunRiseFromWest));
+
+        //Question 8: Initial of your name (Use char)
+        char initialOfMyName = 'R';
+        char initialOfMyLastName = 'T';
+        System.out.println("Initial of my name is " + (initialOfMyName) + (initialOfMyLastName));
+
+        //Question 9: Store your full name
+        String myFullName = "Rukon Talukdar";
+        System.out.println("My full name is " + (myFullName));
+
+    }
